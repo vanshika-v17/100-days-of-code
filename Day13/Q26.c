c897bb8b14c42ec605b039feca12521413c8d0b8
Vanshika <vanshikav1712@gmail.com>
2026-08-22 23:57:44 +0530

    add day13 ques26

---
+++ Day13/Q26.c
@@ -0,0 +1,29 @@
+// Q26: Write a program to print numbers from 1 to n.
+
+/*
+Sample Test Cases:
+Input 1:
+5
+Output 1:
+1 2 3 4 5
+
+Input 2:
+3
+Output 2:
+1 2 3
+
+*/
+
+#include<stdio.h>
+int main() {
+    int n, i = 1;
+
+    printf("enter n");
+    scanf("%d", &n);
+
+    while (i <= n) {
+        printf("%d", i);
+        i++;
+    }
+    return 0;
+}
