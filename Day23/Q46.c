66a27fd0dbab97f09e0d21101712c72bbd93dff8
Vanshika <vanshikav1712@gmail.com>
2026-09-02 21:06:01 +0530

    ques45

---
+++ Day23/Q46.c
@@ -0,0 +1,19 @@
+// Q46: Write a program to print the following pattern:
+/* 
+*****
+*****
+*****
+*****
+*****
+
+Sample Test Cases:
+Input 1:
+
+Output 1:
+*****
+*****
+*****
+*****
+*****
+
+*/
