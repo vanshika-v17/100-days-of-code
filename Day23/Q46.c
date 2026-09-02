d7f49b3c8d2c2b596f607bbbebbb76b3be4d86af
Vanshika <vanshikav1712@gmail.com>
2026-09-02 21:10:28 +0530

    day 23 ques 46

--- Day23/Q46.c
+++ Day23/Q46.c
@@ -17,3 +17,19 @@ Output 1:
 *****
 
 */
+
+#include<stdio.h>
+int main() {
+    int i,j;
+
+
+    for (i = 1; i <= 5; i++)
+    {
+        for (j = 1; j <=5; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
