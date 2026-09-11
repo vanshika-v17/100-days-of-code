15eb5c23e43705f632d5daa1a5473049fd4cabfa
Vanshika <vanshikav1712@gmail.com>
2026-09-11 23:59:41 +0530

    q65

---
+++ Day33/Q65.c
@@ -0,0 +1,19 @@
+/*
+Q65: Search in a sorted array using binary search.
+
+Sample Test Cases:
+Input 1:
+5
+1 3 5 7 9
+7
+Output 1:
+Found at index 3
+
+Input 2:
+5
+1 3 5 7 9
+6
+Output 2:
+-1
+
+*/
