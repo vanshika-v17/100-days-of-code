ed11b059e0c6a077b19c42d7cc2c9ec47f57aa7e
Vanshika <vanshikav1712@gmail.com>
2026-09-12 17:33:31 +0530

    ques66

--- Day33/Q66.c
+++ Day33/Q66.c
@@ -1,1 +1,36 @@
-//
+// Q66: Insert an element in a sorted array at the appropriate position.
+
+/*
+Sample Test Cases:
+Input 1:
+5
+1 2 4 5 6
+3
+Output 1:
+1 2 3 4 5 6
+
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int n, a[100], x, i;
+
+    scanf("%d", &n);
+
+    for(i = 0; i < n; i++)
+        scanf("%d", &a[i]);
+
+    scanf("%d", &x);
+
+    for(i = n - 1; i >= 0 && a[i] > x; i--)
+        a[i + 1] = a[i];
+
+    a[i + 1] = x;
+
+    for(i = 0; i <= n; i++)
+        printf("%d ", a[i]);
+
+    return 0;
+}
