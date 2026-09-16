b5f6e2652d314b9c8b0f8b2d25f4ffdc4c3b491b
Vanshika <vanshikav1712@gmail.com>
2026-09-16 18:12:02 +0530

    ques73

--- Day37/Q73.c
+++ Day37/Q73.c
@@ -10,3 +10,42 @@ Output 1:
 6 15
 
 */
+
+#include <stdio.h>
+
+int main()
+{
+    int r, c;
+    scanf("%d %d", &r, &c);
+
+    int a[r][c];
+    int sum[r];
+
+    // Input matrix
+    for (int i = 0; i < r; i++)
+    {
+        for (int j = 0; j < c; j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    // Find sum of each row
+    for (int i = 0; i < r; i++)
+    {
+        sum[i] = 0;
+
+        for (int j = 0; j < c; j++)
+        {
+            sum[i] = sum[i] + a[i][j];
+        }
+    }
+
+    // Print row sums
+    for (int i = 0; i < r; i++)
+    {
+        printf("%d ", sum[i]);
+    }
+
+    return 0;
+}
