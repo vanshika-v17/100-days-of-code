c982d000853c799ebe221b1fd8155d6eda99effa
Vanshika <vanshikav1712@gmail.com>
2026-09-16 18:38:20 +0530

    q76

--- Day38/Q76.c
+++ Day38/Q76.c
@@ -17,3 +17,50 @@ Output 2:
 False
 
 */
+#include <stdio.h>
+
+int main()
+{
+    int r, c;
+    scanf("%d %d", &r, &c);
+
+    int a[r][c];
+    int symmetric = 1;
+
+    // Input matrix
+    for (int i = 0; i < r; i++)
+    {
+        for (int j = 0; j < c; j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    // A symmetric matrix must be square
+    if (r != c)
+    {
+        symmetric = 0;
+    }
+    else
+    {
+        // Check symmetry
+        for (int i = 0; i < r; i++)
+        {
+            for (int j = 0; j < c; j++)
+            {
+                if (a[i][j] != a[j][i])
+                {
+                    symmetric = 0;
+                    break;
+                }
+            }
+        }
+    }
+
+    if (symmetric == 1)
+        printf("True");
+    else
+        printf("False");
+
+    return 0;
+}
