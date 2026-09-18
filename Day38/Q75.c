32bb0badadf1a18f6a73a3b6a09c42592e7da74c
Vanshika <vanshikav1712@gmail.com>
2026-09-18 08:41:32 +0530

    ques75

--- Day38/Q75.c
+++ Day38/Q75.c
@@ -15,47 +15,5 @@ Output 1:
 
 */
 
-#include <stdio.h>
 
-int main()
-{
-    int r1, c1, r2, c2;
-
-    scanf("%d %d", &r1, &c1);
-
-    int a[r1][c1];
-
-    // Input first matrix
-    for (int i = 0; i < r1; i++)
-    {
-        for (int j = 0; j < c1; j++)
-        {
-            scanf("%d", &a[i][j]);
-        }
-    }
-
-    scanf("%d %d", &r2, &c2);
-
-    int b[r2][c2];
-
-    // Input second matrix
-    for (int i = 0; i < r2; i++)
-    {
-        for (int j = 0; j < c2; j++)
-        {
-            scanf("%d", &b[i][j]);
-        }
-    }
-
-    // Add and print matrices
-    for (int i = 0; i < r1; i++)
-    {
-        for (int j = 0; j < c1; j++)
-        {
-            printf("%d ", a[i][j] + b[i][j]);
-        }
-        printf("\n");
-    }
-
-    return 0;
-}
+    
