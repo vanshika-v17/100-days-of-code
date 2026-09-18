350bae8bad6a8029bce457bbddf642452293b226
Vanshika <vanshikav1712@gmail.com>
2026-09-18 08:37:15 +0530

    ques68

--- Day34/Q68.c
+++ Day34/Q68.c
@@ -10,24 +10,3 @@ Output 1:
 1 2 4 5
 
 */
-#include <stdio.h>
-
-int main()
-{
-    int n, a[100], pos, i;
-
-    scanf("%d", &n);
-
-    for(i = 0; i < n; i++)
-        scanf("%d", &a[i]);
-
-    scanf("%d", &pos);
-
-    for(i = pos; i < n - 1; i++)
-        a[i] = a[i + 1];
-
-    for(i = 0; i < n - 1; i++)
-        printf("%d ", a[i]);
-
-    return 0;
-}
