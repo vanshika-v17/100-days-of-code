d57c3ecffec8228f2302f4c63811356a7b494c5d
Vanshika <vanshikav1712@gmail.com>
2026-09-18 08:31:52 +0530

    q61

--- Day31/Q61.c
+++ Day31/Q61.c
@@ -18,25 +18,7 @@ Output 2:
 
 */
 
-#include <stdio.h>
 
-int main()
-{
-    int n, i, key, index = -1;
-
-    scanf("%d", &n);
-
-    int arr[n];
-
-    for(i = 0; i < n; i++)
-    {
-        scanf("%d", &arr[i]);
-    }
-
-    scanf("%d", &key);
-
-    for(i = 0; i < n; i++)
-    {
         if(arr[i] == key)
         {
             index = i;
