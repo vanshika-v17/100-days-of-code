12aeaef47c12db085c34d6dc492799014f9b9eed
Vanshika <vanshikav1712@gmail.com>
2026-09-18 08:38:35 +0530

    ques70

--- Day35/Q70.c
+++ Day35/Q70.c
@@ -11,31 +11,3 @@ Output 1:
 
 */
 
-#include <stdio.h>
-
-int main()
-{
-    int n, a[100], k, i, j, temp;
-
-    scanf("%d", &n);
-
-    for(i = 0; i < n; i++)
-        scanf("%d", &a[i]);
-
-    scanf("%d", &k);
-
-    for(j = 0; j < k; j++)
-    {
-        temp = a[n - 1];
-
-        for(i = n - 1; i > 0; i--)
-            a[i] = a[i - 1];
-
-        a[0] = temp;
-    }
-
-    for(i = 0; i < n; i++)
-        printf("%d ", a[i]);
-
-    return 0;
-}
